32ca2b51038914e47d66ab89ccbc28ad67f58f69
Murvat <122210081+Murvat@users.noreply.github.com>
2025-10-01 16:56:36 +0200

    .

--- src/app/(pages)/(auth)/confirm/page.tsx
+++ src/app/(pages)/(auth)/confirm/page.tsx
@@ -1,12 +1,7 @@
 import ConfirmEmailContainer from "@/app/domains/auth/containers/ConfirmEmailContainer";
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams?: { email?: string };
-}) {
-  const email = searchParams?.email ?? "your email";
-
+export default function ConfirmPage(props: { searchParams?: { email?: string } }) {
+  const email = props.searchParams?.email ?? "your email";
   return (
     <section className="max-w-lg mx-auto min-h-screen flex flex-col justify-center items-center px-6">
       <ConfirmEmailContainer email={email} />
