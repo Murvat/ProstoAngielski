b48d5e29a1d38426a9b28cdff46762f50a140d00
Murvat <122210081+Murvat@users.noreply.github.com>
2025-10-01 16:44:38 +0200

    fixes

--- src/app/(pages)/(auth)/confirm/page.tsx
+++ src/app/(pages)/(auth)/confirm/page.tsx
@@ -1,12 +1,12 @@
 import ConfirmEmailContainer from "@/app/domains/auth/containers/ConfirmEmailContainer" 
 
 
-export  default async function page({
-    searchParams,
-  }: {
-    searchParams: { email?: string }
-  }) {
-    const email =await searchParams.email ?? "your email"
+export  default  function page({
+  searchParams,
+}: {
+  searchParams: { email?: string };
+}) {
+    const email = searchParams.email ?? "your email"
   
     return (
       <section className="max-w-lg mx-auto min-h-screen flex flex-col justify-center items-center px-6">
