b8ec1d5b9e114254818d810e1f8a36f74c34a17a
Murvat <122210081+Murvat@users.noreply.github.com>
2025-10-01 16:47:27 +0200

    .

--- src/app/(pages)/(auth)/confirm/page.tsx
+++ src/app/(pages)/(auth)/confirm/page.tsx
@@ -1,16 +1,15 @@
-import ConfirmEmailContainer from "@/app/domains/auth/containers/ConfirmEmailContainer" 
+import ConfirmEmailContainer from "@/app/domains/auth/containers/ConfirmEmailContainer";
 
+type ConfirmPageProps = {
+  searchParams?: { email?: string };
+};
 
-export  default  function page({
-  searchParams,
-}: {
-  searchParams: { email?: string };
-}) {
-    const email = searchParams.email ?? "your email"
-  
-    return (
-      <section className="max-w-lg mx-auto min-h-screen flex flex-col justify-center items-center px-6">
-        <ConfirmEmailContainer email={email} />
-      </section>
-    )
-  }
+export default function Page({ searchParams }: ConfirmPageProps) {
+  const email = searchParams?.email ?? "your email";
+
+  return (
+    <section className="max-w-lg mx-auto min-h-screen flex flex-col justify-center items-center px-6">
+      <ConfirmEmailContainer email={email} />
+    </section>
+  );
+}
