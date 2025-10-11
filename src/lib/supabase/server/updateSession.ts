a69aa536a6a182ba7dcf1d11349ca959620b36b0
Murvat <122210081+Murvat@users.noreply.github.com>
2025-10-11 13:41:52 +0200

    reset

--- src/lib/supabase/server/updateSession.ts
+++ src/lib/supabase/server/updateSession.ts
@@ -37,6 +37,7 @@ export async function updateSession(request: NextRequest) {
     path.startsWith("/contact") ||
     path.startsWith("/blog") ||
     path.startsWith("/confirm") ||
+    path.startsWith("/reset") ||
     path.startsWith("/privacy");
 
   // 1️⃣ Not logged in → redirect to /login
